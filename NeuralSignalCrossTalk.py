cd679f63ff5e591a3414e76b22585f4162c34c86
admo7 <35484148+admo7@users.noreply.github.com>
2018-01-21 15:51:29 +0000

    Neuron Positions Test
    Using this branch to test the neuron_positions object.
    
    Get an idea of what it looks like and how to manipulate it.

--- NeuralSignalCrossTalk.py
+++ NeuralSignalCrossTalk.py
@@ -11,8 +11,10 @@ def check_for_conflicts(nerves, conflict_radius):
 
 def gen_coord ():
 	return int(random.random () * NERVE_SIZE)
-
+'''
 if __name__ == ’__main__’:
 	neuron_positions = [[gen_coord (), gen_coord ()] for i in range(NUM_NEURONS)]
 	n_conflicts = check_for_conflicts(neuron_positions, CONFLICT_RADIUS)
 	print " Neurons in conflict : {}".format (n_conflicts)
+'''
+
