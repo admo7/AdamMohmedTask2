21635138d135296a79970efd0f0727756addfa88
admo7 <35484148+admo7@users.noreply.github.com>
2018-01-22 12:15:43 +0000

    Testing
    Check if the x and y coords can be accessed as expected

--- NeuralSignalCrossTalk.py
+++ NeuralSignalCrossTalk.py
@@ -1,10 +1,10 @@
 #import SciPy as DontUseThis
 import random
-random.seed(1) # Setting random number generator seed for repeatability
+random.seed(1) #Setting random number generator seed for repeatability
 
-NUM_NEURONS = 3 # Start with a small no for testing, use original 10000 later
-NERVE_SIZE = 128000 # nanometers 
-CONFLICT_RADIUS = 500 # nanometers
+NUM_NEURONS = 3 #Start with a small no for testing, use original 10000 later
+NERVE_SIZE = 128000 #nanometers 
+CONFLICT_RADIUS = 500 #nanometers
  
 def check_for_conflicts(nerves, conflict_radius):
 	raise NotImplementedError()
@@ -19,9 +19,12 @@ if __name__ == '__main__':
 	print " Neurons in conflict :{}".format (n_conflicts)
 '''
 
-neuron_positions = [[gen_coord(), gen_coord()] for i in range(NUM_NEURONS)] # This gives the coords of each neuron in a 2D list
+neuron_positions = [[gen_coord(), gen_coord()] for i in range(NUM_NEURONS)] #This gives the coords of each neuron in a 2D list
 
 print neuron_positions
 
-for i in neuron_positions: # This should just print the coords of each neuron one at a time
+for i in neuron_positions: #This should just print the coords of each neuron one at a time
 	print i 
+	
+	print i[0] #The x-coord of the current neuron
+	print i[1] #The y-coord of the current neuron
